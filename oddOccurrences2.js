3b9f11ed2addb8fdae90c13e4dc6bbc567f6db50
Desislava Kracheva <desislava.92@abv.bg>
2021-12-08 12:04:47 +0200

    solve the problem for second time

---
+++ oddOccurrences2.js
@@ -0,0 +1,29 @@
+function solveForSecondTime (input){
+let arr = input.toLowerCase().split(' ');
+
+let obj = new Map();
+
+for(let word of arr){
+
+    if(!obj.has(word)){
+
+        obj.set(word,0);
+    } 
+
+    obj.set(word, obj.get(word) + 1);
+
+}
+let keys = Array.from(obj.keys()).filter(x => obj.get(x) %2 !== 0)
+console.log(keys.join(' '));
+
+
+
+
+
+
+
+
+
+}
+
+solveForSecondTime('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
--- wordsTracker2.js
+++ wordsTracker2.js
@@ -1,35 +1,36 @@
-function solveForSecondTime(arr){
+function solveForSecondTime(arr) {
 
     let words = arr.shift().split(' ');
 
     let obj = {};
-    
-    for(let word of words){
 
-        obj[word] =0
+    for (let word of words) {
+
+        obj[word] = 0
 
     }
-        for (let word of arr){
-            if(obj.hasOwnProperty(word)){
-                
-                obj[word]++;
-                }
-                
+    for (let word of arr) {
+        if (obj.hasOwnProperty(word)) {
 
+            obj[word]++;
         }
 
 
-        
-Object.keys(obj).sort((a,b) => obj[b] - obj[a]).forEach(key => {
-    console.log(`${key} - ${obj[key]}`);
-})
-    
+    }
+
+
+
+    Object.keys(obj).sort((a, b) => obj[b] - obj[a])
+        .forEach(key => {
+            console.log(`${key} - ${obj[key]}`);
+        })
+
 
 
 }
 
 solveForSecondTime([
-    'this sentence', 'In','this','sentence','you','have','to','count','the','occurances','of','the'
-    ,'words','this','and','sentence','because','this','is','your','task'
-    ]
-    )
+    'this sentence', 'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurances', 'of', 'the'
+    , 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+]
+)
