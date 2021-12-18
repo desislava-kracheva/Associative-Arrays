80c9bbd5185b770c600f9689a97710ecf588bbc4
Desislava Kracheva <desislava.92@abv.bg>
2021-12-19 00:07:45 +0200

    solve for 2nd time

--- wordOccurrences2.js
+++ wordOccurrences2.js
@@ -1,8 +1,23 @@
 function solveForSecondTime(arr){
 
-    
+    let obj = {}
+   for(let i = 0; i< arr.length; i++){
+if(!obj.hasOwnProperty(arr[i])){
 
+obj[arr[i]] = 1
+
+} else {
+    obj[arr[i]] = obj[arr[i]]+ 1;
 }
-solveForSecondTime(["Here", "is", "the", "first",
- "sentence", "Here", "is", "another", "sentence", "And", 
- "finally", "the", "third", "sentence"])
+
+   }
+
+  let sorted = Object.entries(obj).sort((a,b) => b[1] - a[1]);
+
+
+sorted.forEach(kvp => 
+    console.log(`${kvp[0]} -> ${kvp[1]} times`))
+}
+solveForSecondTime(["Here", "is", "the", "first", "sentence", 
+"Here", "is", "another", "sentence", "And", "finally",
+ "the", "third", "sentence"])
