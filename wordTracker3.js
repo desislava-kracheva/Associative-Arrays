8bdc5ae4d6c6ebbb279cd33a4f5863691bdf9f0a
Desislava Kracheva <desislava.92@abv.bg>
2021-12-19 21:18:06 +0200

    solve for 3rd time

---
+++ wordTracker3.js
@@ -0,0 +1,34 @@
+function solveForThirdTime(arr){
+    let words = arr.shift().split(' ');
+ let obj ={};
+ let counter = 0;
+  
+ for(let word of words){
+     obj[word] = 0;
+ }
+
+ for(let line of arr){
+
+    if(obj.hasOwnProperty(line)){
+
+        obj[line] ++ 
+    }
+ }
+
+let sorted = Object.entries(obj).sort((a,b) => b[1]-a[1] );
+
+sorted.forEach(kvp => {
+
+    console.log(`${kvp[0]} -> ${kvp[1]}`);
+})
+
+}
+
+solveForThirdTime([
+    'this sentence', 
+    'In', 'this', 'sentence', 'you', 'have', 'to', 
+    'count', 'the', 'occurances', 'of', 'the', 'words',
+     'this', 'and', 'sentence', 'because', 'this', 'is', 
+     'your', 'task'
+    ]
+    )
